8e9c66930951943f37026a4ee5f3c4412ceed517
Valera <valerasebafr1@gmail.com>
2013-12-06 00:20:57 +0200

    Add jquery and ready

--- main.js
+++ main.js
@@ -1,45 +1,41 @@
-function initialize() {
-    var mapOptions = {
-        center: new google.maps.LatLng(48.498408, 34.968624),
-        zoom: 6,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map(document.getElementById("map_canvas"),
-        mapOptions);
+/**
+ * @author Valeriy Garzha
+ */
 
+(function ($) {
+    "use strict";
     var markers = [];
 
-    google.maps.event.addListener(map, 'click', function (e) {
-        markers[e.latLng.toString()] = new google.maps.Marker({
-            position: e.latLng,
-            map: map,
-            title: 'Click to delete'
-        });
-        google.maps.event.addListener(markers[e.latLng.toString()], 'click', function (e) {
-            this.setMap(null);
+    $( document ).ready(function() {
+        var mapOptions = {
+            center: new google.maps.LatLng(48.498408, 34.968624),
+            zoom: 6,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map_canvas"),
+            mapOptions);
+
+        google.maps.event.addListener(map, 'click', function (e) {
+            markers[e.latLng.toString()] = new google.maps.Marker({
+                position: e.latLng,
+                map: map,
+                title: 'Click to delete'
+            });
+            google.maps.event.addListener(markers[e.latLng.toString()], 'click', function (e) {
+                this.setMap(null);
+                markers.splice(e.latLng.toString(), 1);
+            });
         });
     });
 
-//            google.maps.event.addListener(markers, 'dblclick', function(e) {
-//                var marker = new google.maps.Marker({
-//                    position: e.latLng,
-//                    map: map,
-//                    title: 'Click to zoom'
-//                });
-//            });
+    $("form").submit(function (event) {
+        if ($("input:first").val() === "correct") {
+            $("span").text("Validated...").show();
+            return;
+        }
 
-//            google.maps.event.addListener(marker, 'click', function() {
-//                map.setZoom(8);
-//                map.setCenter(marker.getPosition());
-//            });
-}
+        $("span").text("Not valid!").show().fadeOut(1000);
+        event.preventDefault();
+    });
 
-$( "form" ).submit(function( event ) {
-    if ( $( "input:first" ).val() === "correct" ) {
-        $( "span" ).text( "Validated..." ).show();
-        return;
-    }
-
-    $( "span" ).text( "Not valid!" ).show().fadeOut( 1000 );
-    event.preventDefault();
-});
+})(jQuery);
